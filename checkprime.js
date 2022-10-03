ffb639ca2f1853877a000372fbc3f5f47b7cdd22
Rajmalpankaj786 <rajmalpankaj786@gmail.com>
2022-10-03 07:54:10 -0700

    creating a function

---
+++ checkprime.js
@@ -0,0 +1,15 @@
+ {
+   let count = 0;
+
+   for(let i=2;i<num-1;i++){
+
+    if(i%num==0{
+    count++;
+}
+
+  if(count == 2){
+    console.log("prime");
+  else {
+    console.log("no prime");
+  }
+  }
---
+++ function_checkprime.js
@@ -0,0 +1,21 @@
+ {
+
+function checkprime(num){
+
+   let count = 0;
+
+   for(let i=1;i<=num;i++){
+
+    if(num%i==0){
+    count++;
+}
+
+  if(count == 2){
+    console.log("prime");
+  else {
+    console.log("no prime");
+  }
+  }
+}
+ 
+   checkprime(13);
